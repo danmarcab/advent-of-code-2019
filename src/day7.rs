97e103738d0a6a94dc35057b1a505010e44cf06b
Daniel Marin <danmarcab@gmail.com>
2019-12-08 10:32:43 +0000

    day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use multimap::MultiMap;
+use std::collections::HashMap;
 use std::io;
 
 #[aoc_generator(day7)]
@@ -14,8 +15,6 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 #[aoc(day7, part1)]
 pub fn part1(program: &[i32]) -> i32 {
     let perms: Vec<Vec<i32>> = (0..5).permutations(5).collect();
-    //    println!("{:?}", perms);
-    //    println!("{:?}", perms.len());
 
     let mut results: MultiMap<Vec<i32>, i32> = MultiMap::new();
 
@@ -25,45 +24,86 @@ pub fn part1(program: &[i32]) -> i32 {
         results.insert(phase_sequence, res);
     }
 
-    if let Some((seq, val)) = results.iter().max_by(|(k, v), (k2, v2)| v.cmp(v2)) {
+    if let Some((_seq, val)) = results.iter().max_by(|(_k, v), (_k2, v2)| v.cmp(v2)) {
         //        println!("{:?} -> {}", seq, val);
         *val
     } else {
         unreachable!()
     }
-
-    //    let mut mut_program: Vec<i32> = program.into();
-    //    run(&mut mut_program, 0);
-    //
-    //    mut_program[0]
 }
 
 fn run_sequence(program: &[i32], phase_sequence: &Vec<i32>) -> i32 {
-    let mut output: Vec<i32> = Vec::new();
-    let input: Vec<i32> = Vec::new();
+    let mut programs: Vec<Vec<i32>> = vec![];
+    let mut inputs: Vec<Vec<i32>> = vec![];
+    let mut outputs: Vec<Vec<i32>> = vec![];
+    let mut pcs: Vec<i32> = vec![];
 
-    let mut input_signal = 0;
+    let mut finished = false;
 
-    for phase in phase_sequence {
-        run_program(program, &vec![*phase, input_signal], &mut output);
-        match output.last() {
-            Some(val) => input_signal = *val,
-            None => unreachable!(),
-        }
+    // initial values
+    for i in 0..5 {
+        programs.push(program.into());
+        inputs.push(vec![phase_sequence[i]]);
+        outputs.push(Vec::new());
+        pcs.push(0);
     }
+    // initial input
+    inputs[0].push(0);
 
-    input_signal
-}
+    let mut iters = 1;
+    while !finished {
+        for i in 0..5 {
+            println!("Executing iter {} fuse {}", iters, i);
+            println!("I: {:?}", inputs);
+            println!("O: {:?}", outputs);
+            println!("pcs: {:?}", pcs);
+            println!("");
 
-fn run_program(program: &[i32], input: &Vec<i32>, output: &mut Vec<i32>) {
-    let mut mut_program: Vec<i32> = program.into();
+            match run(&mut programs[i], pcs[i], &inputs[i], &mut outputs[i]) {
+                Some(new_pc) => {
+                    pcs[i] = new_pc;
+                    inputs[i] = vec![];
+                }
+                None => {
+                    finished = true;
+                }
+            }
 
-    run(&mut mut_program, 0, &input, output);
+            match outputs[i].last() {
+                Some(val) => inputs[(i + 1) % 5].push(*val),
+                None => unreachable!(),
+            }
+        }
+        iters += 1;
+    }
+    println!("Iters {:?}", iters);
+    println!("I: {:?}", inputs);
+    println!("O: {:?}", outputs);
+    println!("pcs: {:?}", pcs);
+
+    *outputs[4].last().unwrap()
 }
 
 #[aoc(day7, part2)]
 pub fn part2(program: &[i32]) -> i32 {
-    part1(program)
+    let perms: Vec<Vec<i32>> = (5..10).permutations(5).collect();
+
+    //    let perms: Vec<Vec<i32>> = vec![vec![5, 6, 7, 8, 9]];
+
+    let mut results: MultiMap<Vec<i32>, i32> = MultiMap::new();
+
+    for phase_sequence in perms {
+        let res = run_sequence(program, &phase_sequence);
+
+        results.insert(phase_sequence, res);
+    }
+
+    if let Some((_seq, val)) = results.iter().max_by(|(_k, v), (_k2, v2)| v.cmp(v2)) {
+        //        println!("{:?} -> {}", seq, val);
+        *val
+    } else {
+        unreachable!()
+    }
 }
 
 // HELPERS
@@ -77,7 +117,7 @@ fn to_digits(n: i32) -> (i32, i32, i32, i32, i32) {
     )
 }
 
-fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) {
+fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) -> Option<i32> {
     let op_code = program[pc as usize];
 
     //    println!("Executing {} at pos: {}", op_code, pc);
@@ -90,7 +130,7 @@ fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) {
             let param_b: i32 = param_value(program, mode_b, pc + 2);
             program[dest_dir as usize] = param_a + param_b;
             //            println!("Adding: {} + {} into {}", param_a, param_b, dest_dir);
-            run(program, pc + 4, input, output);
+            run(program, pc + 4, input, output)
         }
         (0, mode_b, mode_a, 0, 2) => {
             let dest_dir: i32 = program[(pc + 3) as usize];
@@ -98,7 +138,7 @@ fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) {
             let param_b: i32 = param_value(program, mode_b, pc + 2);
             program[dest_dir as usize] = param_a * param_b;
             //            println!("Multiplying: {} * {} into {}", param_a, param_b, dest_dir);
-            run(program, pc + 4, input, output);
+            run(program, pc + 4, input, output)
         }
         (0, 0, 0, 0, 3) => {
             let dest_dir: i32 = program[(pc + 1) as usize];
@@ -110,8 +150,13 @@ fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) {
             //                .expect("failed to read input.");
             //            let n: i32 = buffer.trim().parse().expect("invalid input");
 
-            program[dest_dir as usize] = input[0];
-            run(program, pc + 2, &input[1..], output);
+            if let Some(val) = input.get(0) {
+                program[dest_dir as usize] = *val;
+                run(program, pc + 2, &input[1..], output)
+            } else {
+                //                println!("suspending...");
+                Some(pc)
+            }
         }
         (0, 0, mode, 0, 4) => {
             //            println!("{}", param_value(program, mode, pc + 1));
@@ -119,24 +164,24 @@ fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) {
             let val = param_value(program, mode, pc + 1);
             output.push(val);
 
-            run(program, pc + 2, input, output);
+            run(program, pc + 2, input, output)
         }
         (0, mode_dest, mode_cond, 0, 5) => {
             let cond: i32 = param_value(program, mode_cond, pc + 1);
             let dest: i32 = param_value(program, mode_dest, pc + 2);
             if cond != 0 {
-                run(program, dest, input, output);
+                run(program, dest, input, output)
             } else {
-                run(program, pc + 3, input, output);
+                run(program, pc + 3, input, output)
             }
         }
         (0, mode_dest, mode_cond, 0, 6) => {
             let cond: i32 = param_value(program, mode_cond, pc + 1);
             let dest: i32 = param_value(program, mode_dest, pc + 2);
             if cond == 0 {
-                run(program, dest, input, output);
+                run(program, dest, input, output)
             } else {
-                run(program, pc + 3, input, output);
+                run(program, pc + 3, input, output)
             }
         }
         (0, mode_b, mode_a, 0, 7) => {
@@ -144,16 +189,16 @@ fn run(program: &mut Vec<i32>, pc: i32, input: &[i32], output: &mut Vec<i32>) {
             let param_a: i32 = param_value(program, mode_a, pc + 1);
             let param_b: i32 = param_value(program, mode_b, pc + 2);
             program[dest_dir as usize] = if param_a < param_b { 1 } else { 0 };
-            run(program, pc + 4, input, output);
+            run(program, pc + 4, input, output)
         }
         (0, mode_b, mode_a, 0, 8) => {
             let dest_dir: i32 = program[(pc + 3) as usize];
             let param_a: i32 = param_value(program, mode_a, pc + 1);
             let param_b: i32 = param_value(program, mode_b, pc + 2);
             program[dest_dir as usize] = if param_a == param_b { 1 } else { 0 };
-            run(program, pc + 4, input, output);
+            run(program, pc + 4, input, output)
         }
-        (0, 0, 0, 9, 9) => {}
+        (0, 0, 0, 9, 9) => None,
         other => {
             println!("Invalid Instruction: {:?}", other);
             unreachable!()
@@ -196,6 +241,24 @@ mod tests {
                 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0
             ]),
             65210
+        );
+    }
+    #[test]
+    fn day7part2() {
+        assert_eq!(
+            part2(&[
+                3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28,
+                -1, 28, 1005, 28, 6, 99, 0, 0, 5
+            ]),
+            139629729
+        );
+        assert_eq!(
+            part1(&[
+                3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001,
+                54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53,
+                55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10
+            ]),
+            18216
         );
     }
 }
